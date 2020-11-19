015b70fcebbf3935c9a71514d98351a99e384cbf
Maxmanich <maxmanich@yandex.ru>
2020-11-19 05:30:36 +0300

    All id done

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
-from flask import render_template
-from data import *
+from flask import Flask, render_template
+
+from data import title, subtitle, description, departures, tours
 
 
 app = Flask(__name__)
@@ -8,18 +8,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def render_main():
-    return render_template('index.html', tours=tours, subtitle=subtitle, description=description)
+    return render_template('index.html',
+                           title=title,
+                           tours=tours,
+                           subtitle=subtitle,
+                           description=description,
+                           departures=departures)
 
 
 @app.route('/departures/<departure>/')
 def render_departures(departure):
-    return render_template('departure.html', departure=departure, departures=departures, tours=tours)
+    needed_tours = {}
+    for tour_number, tour_info in tours.items():
+        if tour_info.get('departure') == departure:
+            needed_tours.update({tour_number: tour_info})
+
+    return render_template('departure.html',
+                           title=title,
+                           departure=departure,
+                           departures=departures,
+                           tours=needed_tours)
 
 
 @app.route('/tours/<id>/')
 def render_tours(id):
-    return render_template('tour.html', tour=tours.get(int(id)), departures=departures)
+    return render_template('tour.html',
+                           title=title,
+                           tour=tours.get(int(id)),
+                           departures=departures)
 
 
-if __name__ == '__main__':
-    app.run()
+#if __name__ == '__main__':
+app.run(debug=True)
